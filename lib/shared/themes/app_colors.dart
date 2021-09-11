90eed57af158a85538225b3c55abac307f56b39b
Marcelo Freire <marcelofreire@sas.com.br>
2021-09-11 17:30:31 -0300

    Add base files to the app

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFFF941A);
+  static const secondary = Color(0xFF585666);
+  static const background = Color(0xFFFFFFFF);
+  static const heading = Color(0xFF585666);
+  static const body = Color(0xFF706E7A);
+  static const input = Color(0xFFB1B0B8);
+  static const delete = Color(0xFFE83F5B);
+  static const stroke = Color(0xFFE3E3E5);
+  static const shape = Color(0xFFFAFAFC);
+}
